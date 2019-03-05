e83a090fed3c1f03b54ba592c9c437ab26860501
Rich Nakasato <richnakasato@hotmail.com>
2019-03-04 21:25:06 -0500

    had to read this to implement

--- 10.1.sorted_arrays_merge.0.py
+++ 10.1.sorted_arrays_merge.0.py
@@ -1,9 +1,24 @@
 from test_framework import generic_test
 
+import heapq
+
 
 def merge_sorted_arrays(sorted_arrays):
     # TODO - you fill in here.
-    return []
+    arr_iters = [iter(x) for x in sorted_arrays]
+    merged = list()
+    for idx,it in enumerate(arr_iters):
+        elem = next(it, None)
+        if elem is not None:
+            heapq.heappush(merged, (elem, idx))
+    results = list()
+    while merged:
+        elem, idx = heapq.heappop(merged)
+        results.append(elem)
+        elem = next(arr_iters[idx], None)
+        if elem is not None:
+            heapq.heappush(merged, (elem, idx))
+    return results
 
 
 if __name__ == '__main__':
