0734266bbc05222f8e112486504fc532be83d941
Rich Nakasato <richnakasato@hotmail.com>
2019-03-26 23:17:30 -0400

    implemented

--- 12.5.nearest_repeated_entries.0.cc
+++ 12.5.nearest_repeated_entries.0.cc
@@ -1,12 +1,28 @@
+#include <algorithm>
 #include <string>
+#include <unordered_map>
 #include <vector>
+
 #include "test_framework/generic_test.h"
+
+using std::min;
 using std::string;
+using std::unordered_map;
 using std::vector;
 
 int FindNearestRepetition(const vector<string>& paragraph) {
   // TODO - you fill in here.
-  return 0;
+  if (paragraph.empty()) return -1;
+  unordered_map<string,int> memo;
+  int closest = paragraph.size()+1;
+  for (int i=0; i<paragraph.size(); ++i) {
+      string word = paragraph[i];
+      if (memo.find(word) != memo.end()) {
+          closest = min(closest, i - memo[word]);
+      }
+      memo[word] = i;
+  }
+  return closest > paragraph.size() ? -1 : closest;
 }
 
 int main(int argc, char* argv[]) {
